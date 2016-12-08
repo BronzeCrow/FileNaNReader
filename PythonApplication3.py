5f86cdadb62efd8edb2434feccd5886c9d9e0f2e
Nikita Mashalov <nickita2000@gmail.com>
2016-12-08 23:17:43 +0500

    Add files via upload

--- PythonApplication3.py
+++ PythonApplication3.py
@@ -1,2 +1,30 @@
-abs='fagergrob'
-print(abs[1:3])
+code = bytearray()
+
+with open('weather.data','rb') as f:
+    data=f.read()
+double=[]
+oldscode = 0
+for i in range(0,len(data),32):
+    strg = ''
+    for j in range (0,24):
+        if data[i+j] != 0:
+           strg = strg + chr(data[i+j])
+    bte1 = data[i+31] & 0x7f
+    bte2 = data[i+30] & 0xf0
+    if bte1 == 0x7f and bte2 == 0xf0 :
+        number = data[i+24:i+31]
+        scode = int.from_bytes(number, byteorder='little')
+        scode &= 0x0fffffffffffff
+        scode |= oldscode
+        mask = 0xff
+        mask <<= 44
+
+        for k in range(0,6):
+            symb = scode & mask
+            symb >>= ((5 - k) * 8 + 4)
+            print (symb)
+            code.append(symb)
+            mask >>= 8
+        oldscode = scode & 0xf
+        oldscode <<= 52 
+print (code)
